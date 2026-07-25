62c3f9fdd956689b0ba69736df4119a3a2a9c4ee
Jimmy Sul <52802395+jimmyherosul@users.noreply.github.com>
2026-07-25 12:25:53 +0100

    Update energy_eigenvalues.py

--- 2.Static_coupled_qubit_gates/Physics/energy_eigenvalues.py
+++ 2.Static_coupled_qubit_gates/Physics/energy_eigenvalues.py
@@ -1,1 +1,14 @@
+import numpy as np
 
+def energy_eigenvalues(V_mag, Delta_range, Delta, E_mean):
+    E_plus = E_mean + np.sqrt(Delta_range**2 + V_mag**2)
+    E_minus = E_mean - np.sqrt(Delta_range**2 + V_mag**2)
+    E_plus_current = E_mean + np.sqrt(Delta**2 + V_mag**2)
+    E_minus_current = E_mean - np.sqrt(Delta**2 + V_mag**2)
+
+    return {
+        "E_plus": E_plus,
+        "E_minus": E_minus,
+        "E_plus_current": E_plus_current,
+        "E_minus_current": E_minus_current
+    }
